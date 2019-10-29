a38484308babd5894a147d898db339b5a80f3553
lse13 <55997422+lse13@users.noreply.github.com>
2019-10-29 16:33:24 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,24 @@
+# Exercise 1: Work with the person next to you to design 
+# classes to manage the products, customers, and purchase 
+# orders for an online book store such as amazon.com. 
+# Outline the data attributes and useful methods for 
+# each class. You can discuss and create the outline together. 
+
+
+class Amazon_orders(object):
+  def __init__(self):
+    self.products=None 
+    self.customers=None
+    
+  def add_products(self,product_list):
+    self.product.append(product_list)
+  
+  def get_products(self):
+    return self.products
+  
+  def get_customers(self):
+    return self.customers
+
+  def set_customers(self, customers_list):
+    self.customers = customers_list  
+
